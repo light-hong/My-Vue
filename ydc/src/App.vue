26838d99e7c9166188fb1c817c31d8570f10dcde
Hongliang0513 <1195528273@qq.com>
2019-06-20 10:13:24 +0800

    changed

--- ydc/src/App.vue
+++ ydc/src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <h1>一点车</h1>
     </div>
     <router-view/>
   </div>
